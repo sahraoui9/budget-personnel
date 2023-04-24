244cbdbfa683c54c1a0546d1c80a636e3ed5a6a4
PACHECO Alfredo <54375691+golsmea@users.noreply.github.com>
2023-04-24 16:02:39 +0200

    add Objectif entity

---
+++ api/src/main/java/bgpersonnel/budget/objectif/Objectif.java
@@ -0,0 +1,33 @@
+package bgpersonnel.budget.objectif;
+
+import bgpersonnel.budget.authentification.entity.User;
+import bgpersonnel.budget.category.Category;
+import bgpersonnel.budget.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Objectif extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private double amount;
+    private String description;
+
+    private LocalDateTime term;
+
+    private boolean isReached;
+
+    @ManyToMany(mappedBy = "objectifs")
+    private List <Category> categories = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable=false)
+    private User user;
+}
