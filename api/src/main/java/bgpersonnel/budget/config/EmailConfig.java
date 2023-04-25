f1fd7308607e422c284fb4f8a5cc78f39732efe9
AlexT47 <alexandre.ternois@gmail.com>
2023-04-25 16:56:51 +0200

    Add mail config

---
+++ api/src/main/java/bgpersonnel/budget/config/EmailConfig.java
@@ -0,0 +1,44 @@
+package bgpersonnel.budget.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+@Configuration
+@ConfigurationProperties
+public class EmailConfig {
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.user}")
+    private String user;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        // Set up Gmail config
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+
+        // Set up email config (using udeesa email)
+        mailSender.setUsername(user);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", true);
+        return mailSender;
+    }
+}
