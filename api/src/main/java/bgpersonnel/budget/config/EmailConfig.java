43ab9d9d9e4e25b99bc015ec336794c310e3e4d3
sahraoui.lahcen <lahcen.sahraoui@protonmail.com>
2023-04-30 20:16:23 +0200

    Add docker

--- api/src/main/java/bgpersonnel/budget/config/EmailConfig.java
+++ api/src/main/java/bgpersonnel/budget/config/EmailConfig.java
@@ -18,7 +18,7 @@ public class EmailConfig {
     @Value("${spring.mail.port}")
     private int port;
 
-    @Value("${spring.mail.user}")
+    @Value("${spring.mail.username}")
     private String user;
 
     @Value("${spring.mail.password}")
